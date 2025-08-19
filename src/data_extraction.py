2d1c251d90de356d7679f9f364029bbbe034af02
mai <miaoesh@gmail.com>
2025-08-19 10:03:49 +0300

    Revert "adding sectors for companies"
    This reverts commit 78b217a3dd66e6c0eb176e4925fb3c499d295737.

--- src/data_extraction.py
+++ src/data_extraction.py
@@ -2,12 +2,12 @@ import json
 import requests
 
 
-company_symbols = ["JNJ", "PFE","ABT","CVS","UNH", "META"]
+company_symbols = ["JNJ", "PFE","ABT","CVS","UNH"]
 financial_statements = ["BALANCE_SHEET", "INCOME_STATEMENT", "CASH_FLOW","OVERVIEW"]
 
 for company_symbol in company_symbols:
     for statement in financial_statements:
-        url = f'https://www.alphavantage.co/query?function={statement}&symbol={company_symbol}&apikey=1M7X2X1CUZNAYNDA'
+        url = f'https://www.alphavantage.co/query?function={statement}&symbol={company_symbol}&apikey=1XNOZA5CSL7306MI'
         r = requests.get(url)
         data = r.json()
         # save to a json file
